5d3c81ef8fb552d7b73dfa7709d71663c49609a0
VuokkoH <vuokko.heikinheimo@helsinki.fi>
2019-09-12 12:08:47 +0300

    organized scripts for pulling autograding exercises

--- pull_exercise_repos.py
+++ pull_exercise_repos.py
@@ -2,13 +2,11 @@
 """
 
 Pull GitHub Repositories of Automating GIS processes / Python for Geo-people based on a 
-list of usernames. Organizes repositories in a way that NBgrader can be used to automatically 
+list of exercise numbers. Organizes repositories in a way that NBgrader can be used to automatically
 grade the assignments. This means that:
     
-    - Repositories are inserted into 'submitted' folder
-    - In the submitted folder there will be a subfolder for each student, e.g. 'htenkanen' 
-    - If nbgrader style is used, the GitHub Classroom name is changed from 'exercise-3-htenkanen' to 'Exercise-3' having a full path of './submitted/htenkanen/Exercise-3'
-    - Notice: if nbgrader style is used, .git file is removed from the directory which conflicts with the nbgrader (at least on Windows)
+    - Repositories are inserted into 'source' and 'release' folders
+    - In these folder there will be a subfolder for each exercise eg. 'Exercise-2'
 
 Requirements:
     
@@ -21,6 +19,8 @@ Notes:
 Created on Wed Jan 11 13:55:44 2017
 
 @author: hentenka
+
+Modified Thu Sept 12 2019
 """
 
 from git import Repo
@@ -60,15 +60,6 @@ def pull_repo(repo, github_remote):
         print("Could not pull following remote: %s" % github_remote)
         print(e)
 
-def create_student_folder(base_f, uname):
-    """Creates a folder for a student based on username if it does not exist."""
-    # Student folder
-    student_f = os.path.join(base_f, uname)
-        
-    # Check if the folder exists, if not create one
-    if not os.path.isdir(student_f):
-        os.mkdir(student_f)
-    return student_f
 
 def is_git_repo(directory):
     """Validates if directory is Git repository"""
@@ -106,17 +97,6 @@ def is_classroom_release(repo, exercise):
     else:
         return False
 
-def is_student_classroom(repo, user, exercise):
-    """Validates if repository is an actual remote for the Exercise source files. Returns the remote if it is."""
-    # Check remotes
-    url = repo.remotes.origin.url
-    # Validate 
-    split = url.split('/')
-    assert split[-2] == organization, "Organization of the remote does not match with the project!"
-    if split[-1] == "exercise-%s-%s.git" % (exercise, user):
-        return True
-    else:
-        return False
 
 def update_autograding_source_repo(base_folder, enumber):
     """Clones / pulls the autograding source files"""
@@ -169,71 +149,7 @@ def update_autograding_release_repo(base_folder, enumber):
     # If not clone it
     else:
         git_clone(github_remote=generate_github_remote(organization, "Exercise-%s" % (enumber)), repo_path=exercise_dir)
-        
-def update_course_repo(student_folder, organization, user=None, exercise=None):
-    """Update course repository from GitHub"""
-    
-    # Directory name locally
-    directory_path = os.path.join(student_folder, "Exercise-%s" % exercise)
-    
-    # Check if exercise repository is initialized
-    if is_git_repo(directory_path):
 
-        # If repository exists locally it can be either format "Exercise-3" or "exercise-3-username"
-        repo_path = os.path.join(student_folder, "exercise-%s-%s" % (exercise, user))
-        
-        if os.path.exists(repo_path):
-            pass
-        else:
-            repo_path = os.path.join(student_folder, "Exercise-%s" % (exercise))
-        
-        # Get the repo
-        repo = Repo(repo_path)
-        
-        # Check if the repo is correct student Classroom
-        if user is not None:
-            if is_student_classroom(repo, user, exercise):
-                # Remote url
-                url = repo.remotes.origin.url
-                print("Updating Exercise files of %s" % url)
-                pull_repo(repo, url)
-            else:
-                raise ValueError("%s directory contains something else than the studnet files for Exercise-%s. Please check and ensure that the directory contains valid materials." % (repo_path, exercise))
-                
-    else:
-        # If repository has not been cloned yet
-        # -------------------------------------
-        
-        # Remove Exercise directory if it exists (so that it can be updated)
-        remove_normal_directory(directory_path)
-        
-        # Repository name in GitHub
-        repo_name = "exercise-%s-%s" % (exercise, user)
-        
-        # Local Git Repository
-        repo_path = os.path.join(student_folder, repo_name)
-        
-        # Create origin
-        github_remote = generate_github_remote(organization, repo_name)
-        
-        # If cloning it should be done on the parent folder of the 'repo_path'
-        was_found = git_clone(github_remote, repo_path)
-        
-        # If the student has not started the assignment the repo path does not exist
-        if was_found is False:
-            repo_path = None
-        
-    return repo_path
-
-def create_submitted_folder(base_f):
-    """Creates a submitted folder if it does not exist."""
-    # Submitted folder
-    submitted_f = os.path.join(base_f, "submitted")
-    
-    # Check if the folder exists, if not create one
-    if not os.path.isdir(submitted_f):
-        os.mkdir(submitted_f)
-    return submitted_f
 
 def create_source_exercise_folder(base_f, exercise_num):
     """Creates a submitted folder if it does not exist."""
@@ -371,80 +287,7 @@ def main():
             
             # Add release files ('nbgrader assign')
             assigned = create_assignment(base_folder, enumber)
-            
-            # Create submitted folder if it does not exist
-            submitted_f = create_submitted_folder(base_folder)
-            
-            # Iterate over usernames
-            for uname in user_names:
-        
-                print("Gathering repositories of: %s" % uname)
-            
-                # Parse user directory and create it if it does not exist
-                student_f = create_student_folder(submitted_f, uname)
-                
-                # Update Exercise repository (clone or pull)
-                repo_path = update_course_repo(student_f, organization, user=uname, exercise=enumber)
-                
-                # Change the folder name if the repo was successfully cloned
-                if repo_path:
-                    if use_nbgrader_style:
-                        # Convert the directory name into format supported by nbgrader
-                        new_path = rename_directory_for_nbgrader(repo_path=repo_path, exercise_number=enumber)
-                        
-                        # Remove .git file on Windows so that it does not conflict with nbgrader
-                        if 'win' in platform:
-                            remove_git_folder(new_path)
-                
-                # If the student's exercise was not found, write a log
-                else:
-                    print("Student", uname, "has not started the Exercise", enumber)
-                    # Log to file
-                    log_missing_repos(enumber, uname)
-                
-    
-    # Iterate over any other Github Classroom repos if they are defined
-    if len(additional_classroom_repos) > 0:
-        for add in additional_classroom_repos:
-            
-            # Create submitted folder if it does not exist
-            submitted_f = create_submitted_folder(base_folder)
-            
-            # Parse user directory and create it if it does not exist
-            student_f = create_student_folder(submitted_f, uname)
-            
-            # Repository name
-            repo_name = "%s-%s" % (add, uname)
-            
-            # Local Git Repository
-            repo_path = os.path.join(student_f, repo_name)
-            
-            # CLone repository
-            git_clone(repo_path=repo_path, github_remote=generate_github_remote(organization=organization, repository_name=repo_name))
-        
-        print("Successfully gathered repositories for: %s" % uname)
-        
-    # Iterate over any other Github repos if they are defined and pull them under the base_f 
-    if len(extra_repos) > 0:
-        print("Gathering extra repos ...")
-            
-        for repo_name in extra_repos:
-            print("Gathering repo: %s" % repo_name)
-            
-            # Create submitted folder if it does not exist
-            submitted_f = create_submitted_folder(base_folder)
-                
-            # Parse user directory and create it if it does not exist
-            student_f = create_student_folder(submitted_f, uname)
-            
-            # Local Git Repository
-            repo_path = os.path.join(student_f, repo_name)
-            
-            # CLone repository
-            git_clone(repo_path=repo_path, github_remote=generate_github_remote(organization=organization, repository_name=repo_name))
         
     
 if __name__ == '__main__':
     main()
-        
-        

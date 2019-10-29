e5b370c263abb34eb7a254436c7ee638cd73b1fb
Dave Whipp <davewhipp@users.noreply.github.com>
2019-10-29 11:18:42 +0200

    Minor edit

--- edit_readme.py
+++ edit_readme.py
@@ -147,7 +147,7 @@ def edit_readme(path, score_dict, exercise_num, name):#, html_link):
 
 	file = open(file_path,'a')
 	total = score_dict["Exercise "+str(exercise_num)]
-	file.write("## Grading (by "+ inspector_user_name+ "): "+ total[0]+ " / "+ \
+	file.write("\n## Grading (by "+ inspector_user_name+ "): "+ total[0]+ " / "+ \
 		total[1]+ " points for exercise "+ str(exercise_num)+ "\n")
 	for problem in score_dict:
 		if "Exercise" not in problem:

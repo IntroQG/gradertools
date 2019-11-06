1de3afba9bfe548e5c1a15916aed8c03184f6929
Dave Whipp <davewhipp@users.noreply.github.com>
2019-11-06 13:57:35 +0200

    Test fixing small error in student list

--- graderconfig/tools_conf.py
+++ graderconfig/tools_conf.py
@@ -80,8 +80,7 @@ autograding_suffix = "-autograding"
 #line"""
 
 # Placeholder for graderbot (comment out if using list above)
-user_names = """NAMES_GO_HERE
-"""
+user_names = """NAMES_GO_HERE"""
 
 user_names = user_names.split("\n")
 

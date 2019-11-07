270fed437781f14942dc006f85570951db068902
Dave Whipp <david.whipp@helsinki.fi>
2019-11-07 15:19:55 +0200

    Update output formatting for all exercises

--- edit_readme.py
+++ edit_readme.py
@@ -152,15 +152,20 @@ def edit_readme(path, score_dict, exercise_num, name):#, html_link):
 
 	file = open(file_path,'a')
 	total = score_dict["Exercise "+str(exercise_num)]
-	file.write("\n## Grading (by "+ inspector_user_name+ "): "+ total[0]+ " / "+ \
-		total[1]+ " points for exercise "+ str(exercise_num)+ "\n")
+	file.write("## Exercise {0} grade and feedback: {1:.1f} / {2:.1f} points\n".format(exercise_num, total[0], total[1]))
+	file.write("### Grader\n")
+	file.write("- {0}\n".format(inspector_user_name))
+	file.write("### Problem scores\n")
 	for problem in score_dict:
 		if "Exercise" not in problem:
-			file.write("### "+problem+" - ")
+			file.write("- "+problem+": ")
 			file.write(score_dict[problem][0]+" / "+score_dict[problem][1]+" \n")
 	# if html_link:
 	# 	file.write("### Go to the feedback file [Exercise-"+str(exercise_num)+ \
 	# 		"-feedback.html](Exercise-"+str(exercise_num)+"-feedback.html)")
+	file.write("### Comments\n")
+	file.write("- Comments will be added here.\n")
+
 	return
 
 def main():

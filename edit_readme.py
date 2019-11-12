4f267193cc7a0680255660cec98bca8de2232de1
Dave Whipp <davewhipp@users.noreply.github.com>
2019-11-13 00:42:20 +0200

    Remove special handling of Exercise 2

--- edit_readme.py
+++ edit_readme.py
@@ -12,45 +12,6 @@ import re
 import os
 from graderconfig.tools_conf import base_folder, user_names, exercise_list, inspector_user_name
 
-def read_html_ex2(path, exercise, name):
-	file_path = os.path.join(path, "Exercise-"+str(exercise)+"-feedback.html")
-
-	# Print feedback file info, student name, exercise number
-	print("Getting points for user", name, "exercise", exercise)
-	print(file_path)
-
-	try:
-		feedbackhtml = open(file_path,'rb').readlines()
-	except FileNotFoundError:
-		print("html feedback not found. Generate it for", name, "exercise", exercise)
-		return {}
-
-	# Set up regular expression to remove html tags
-	rmtags = re.compile('<.*?>')
-	score_dict = {}
-	scores = [line.decode('ISO-8859-1') for line in feedbackhtml \
-		if ('(Score:' in line.decode('ISO-8859-1'))]
-
-	# Check whether it's able to fetch scores from html feedback
-	read_next = True
-	problem = 0
-	index = 1
-	while (read_next):
-		strip_score = re.sub(rmtags, '', scores[index]).strip().split(" ")
-		if "problem_"+str(problem) in scores[index]:
-			if "Problem "+str(problem) in score_dict:
-				score_dict["Problem "+str(problem)].append([strip_score[-3],strip_score[-1][:-1]])
-			index += 1
-		else:
-			problem += 1
-			score_dict["Problem "+str(problem)] = []
-		if index >= len(scores):
-			read_next = False
-	strip_total = re.sub(rmtags, '', scores[0]).strip().split(" ")
-	score_dict["Exercise "+str(exercise)] = [[strip_total[-3],strip_total[-1][:-1]]]
-
-	return score_dict
-
 def read_html(path, exercise, name):
 	file_path = os.path.join(path, "Exercise-"+str(exercise)+"-feedback.html")
 
@@ -106,36 +67,6 @@ def read_html(path, exercise, name):
 
 	return score_dict
 
-def edit_readme_ex2(path, score_dict, exercise_num, name):#, html_link):
-	file_path = os.path.join(path,"README.md")
-
-	# Print README.md file info
-	print("Editing readme for student", name, "Exercise", exercise_num)
-	print(file_path)
-
-	file = open(file_path,'a')
-	total = score_dict["Exercise "+str(exercise_num)][0]
-	file.write("## Exercise {0} grade and feedback: {1} / {2} points\n".format(exercise_num, total[0], total[1]))
-	file.write("### Grader\n")
-	file.write("- {0}\n".format(inspector_user_name))
-	file.write("### Problem scores\n")
-
-	for problem in score_dict:
-		if "Exercise" not in problem:
-			file.write("- "+problem+": ")
-			score = 0
-			total = 0
-			for score_list in score_dict[problem]:
-				score += float(score_list[0])
-				total += float(score_list[1])
-			file.write(str(score)+" / "+str(total)+"\n")
-	# if html_link:
-	# 	file.write("### Go to the feedback file [Exercise-"+str(exercise_num)+ \
-	# 		"-feedback.html](Exercise-"+str(exercise_num)+"-feedback.html)")
-	file.write("### Comments\n")
-	file.write("- Comments will be added here.\n")
-	return
-
 def edit_readme(path, score_dict, exercise_num, name):#, html_link):
 	file_path = os.path.join(path,"README.md")
 
@@ -184,18 +115,11 @@ def main():
 			# else:
 			# 	html_link = True
 			file_path = os.path.join(submitted_f, "Exercise-"+str(exercise))
-			if exercise == 2:
-				scores_dict = read_html_ex2(file_path, exercise, name)
-				if scores_dict != {}:
-					edit_readme_ex2(file_path, scores_dict, exercise, name)#, html_link)
-				else:
-					print("Unable to fetch scores.")
-			else: 
-				scores_dict = read_html(file_path, exercise, name)
-				if scores_dict != []:
-					edit_readme(file_path, scores_dict, exercise, name)#, html_link)
-				else:
-					print("Unable to fetch scores.")
+			scores_dict = read_html(file_path, exercise, name)
+			if scores_dict != []:
+				edit_readme(file_path, scores_dict, exercise, name)#, html_link)
+			else:
+				print("Unable to fetch scores.")
 
 if __name__ == '__main__':
     main()
